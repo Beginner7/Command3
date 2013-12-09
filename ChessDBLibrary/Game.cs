23f0f999f1a21b10f39e788ebd73a4f1f4709538
Marina Vyalova <klap.mar8m@yandex.ru>
2013-12-09 18:31:06 +0400

    added Game class and Gamer class to work with the database

---
+++ ChessDBLibrary/Game.cs
@@ -0,0 +1,63 @@
+﻿using ChessDBLibrary.ChessDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessDBLibrary
+{
+    class Game
+    {
+        ChessDataSet.GamesDataTable gamesTable;
+        GamesTableAdapter gamesAdapter;
+
+        public Game()
+        {
+            gamesTable = new ChessDataSet.GamesDataTable();
+            gamesAdapter = new GamesTableAdapter();
+        }
+
+        public void AddNewGame(int blackPlayerID, int whitePlayerID, int creatorID, System.DateTime startDateTime, int timeControl, int gameStatus,
+                                    string movesHistory, string boardState, int whiteRestTime, int blackRestTime)
+        {
+            try
+            {
+                ChessDataSet.GamesRow newRow = gamesTable.NewGamesRow();
+
+                newRow.BlackPlayerID = blackPlayerID;
+                newRow.WhitePlayerID = whitePlayerID;
+                newRow.CreatorID = creatorID;
+                newRow.StartDateTime = startDateTime;
+                newRow.TimeControl = timeControl;
+                newRow.GameStatus = gameStatus;
+                newRow.MovesHistory = movesHistory;
+                newRow.BoardState = boardState;
+                newRow.WhiteRestTime = whiteRestTime;
+                newRow.BlackRestTime = blackRestTime;
+
+                gamesTable.AddGamesRow(newRow);
+                gamesAdapter.Update(gamesTable);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void RemoveGameByID(int id)
+        {
+            try
+            {
+                gamesAdapter.Fill(gamesTable);
+                ChessDataSet.GamesRow rowToDelete = gamesTable.FindByGameID(id);
+                gamesAdapter.Delete(rowToDelete.GameID, rowToDelete.WhitePlayerID, rowToDelete.BlackPlayerID, rowToDelete.CreatorID,
+                                        rowToDelete.StartDateTime, rowToDelete.TimeControl, rowToDelete.GameStatus, rowToDelete.WhiteRestTime, rowToDelete.BlackRestTime);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ChessDBLibrary/Gamer.cs
@@ -0,0 +1,83 @@
+﻿using ChessDBLibrary.ChessDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessDBLibrary
+{
+    class Gamer
+    {
+        ChessDataSet.GamersDataTable gamersTable;
+        GamersTableAdapter gamersAdapter;
+
+        public Gamer()
+        {
+            gamersTable = new ChessDataSet.GamersDataTable();
+            gamersAdapter = new GamersTableAdapter();
+        }
+
+        public void AddNewGamer(string name, string surname, string nickname, string country, System.DateTime birthday,
+                                string eMailAddress, string password, int elo, bool status)
+        {
+            try
+            {
+                ChessDataSet.GamersRow newRow = gamersTable.NewGamersRow();
+
+                newRow.Name = name;
+                newRow.Surname = surname;
+                newRow.Nickname = nickname;
+                newRow.Country = country;
+                newRow.Birthday = birthday;
+                newRow.EMailAddress = eMailAddress;
+                newRow.Password = password;
+                newRow.Elo = elo;
+                newRow.Status = status;
+
+                gamersTable.AddGamersRow(newRow);
+                gamersAdapter.Update(gamersTable);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void RemoveGamerByID(int id)
+        {
+            try
+            {
+                gamersAdapter.Fill(gamersTable);
+                ChessDataSet.GamersRow rowToDelete = gamersTable.FindByGamerID(id);
+                gamersAdapter.Delete(rowToDelete.GamerID, rowToDelete.Name, rowToDelete.Surname, rowToDelete.Nickname, rowToDelete.Country, rowToDelete.Birthday,
+                                        rowToDelete.EMailAddress, rowToDelete.Password, rowToDelete.Elo, rowToDelete.Status);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public int GamerAuthorization(string nickname, string password)
+        {
+            gamersTable = gamersAdapter.GetDataByNickname(nickname);
+
+            if (gamersTable.Rows.Count != 0)
+            {
+                if (password == gamersTable.Rows[0][7].ToString())
+                {
+                    return 0; //Пароль правильный                   
+                }
+                else
+                {
+                    return 1; //Пароль не правильный                   
+                }
+            }
+            else
+            {
+                return 2; //Запись не найдена              
+            }
+        }
+    }
+}
